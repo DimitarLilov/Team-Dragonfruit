8eca01118fa38656ad2e27a8210c02329df627bb
vanncho <van_cho@abv.bg>
2017-09-12 13:36:51 +0300

    Fix delete ticket from cart not worked

--- scripts/models/cartService.js
+++ scripts/models/cartService.js
@@ -1,18 +1,25 @@
 let cartService = (() => {
 
     function getTicketByUserId(userId) {
-       return requester.get('appdata',`cart?query={"userId":"${userId}"}`,'kinvey')
+
+       return requester.get('appdata', `cart?query={"userId":"${userId}"}`, 'kinvey')
     }
-    function getTicketByEventId(eventId) {
-        return requester.get('appdata','cart','kinvey');
+
+    function getTicketByEventId() {
+
+        return requester.get('appdata', 'cart', 'kinvey');
     }
+
     function addTicketCart(data) {
-        return requester.post('appdata', `cart`, 'master',data);
+
+        return requester.post('appdata', `cart`, 'master', data);
     }
 
     function deleteTicket(ticketId) {
-        return requester.remove('appdata','cart/'+ticketId,'kinvey');
+
+        return requester.remove('appdata', `cart/${ticketId}`, 'master');
     }
+
     return {
         addTicketCart,
         getTicketByEventId,
